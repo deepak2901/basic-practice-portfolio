ab4b2052073010386f90178387e59fdcfe519715
deepak <deepak.kondapalli29@gmail.com>
2022-12-27 17:03:37 +0530

    counter,location update

--- counter.js
+++ counter.js
@@ -6,8 +6,27 @@ function updateCounter() {
      * if count is null then set count to 1
     * else increment the count by 1 (Note: count is a string so convert it to number using parseInt)
     */
-
+    if(count == null){
+        count = 1;
+    }
+    else{
+        count++;
+    }
     localStorage.setItem('visiterCount', count);
     counter.innerHTML = `Vistor Count: ${localStorage.getItem('visiterCount')}`;
 }
 updateCounter();
+
+function updateLocation(){
+    const locti = document.getElementById('locti');
+    let location = localStorage.getItem('visiterLocation');
+    if (location == null){
+        location = 1;
+    }
+    else{
+        location++
+    }
+    localStorage.setItem('visiterLocation', location);
+    locti.innerHTML = `Vistor Location: ${localStorage.getItem('visiterLocation')}`;
+}
+updateLocation();
