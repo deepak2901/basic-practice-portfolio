a54a7109b9ad5c9ab0db050b74838fe919b444aa
deepak <deepak.kondapalli29@gmail.com>
2022-12-30 12:39:13 +0530

    check point 4

--- counter.js
+++ counter.js
@@ -6,20 +6,17 @@ function updateCounter() {
      * if count is null then set count to 1
     * else increment the count by 1 (Note: count is a string so convert it to number using parseInt)
     */
-    if(count == null){
+
+    if(count == String(null)){
         count = 1;
     }
     else{
-        count++;
+        count = parseInt(count)+1;
     }
     localStorage.setItem('visiterCount', count);
     counter.innerHTML = `Vistor Count: ${localStorage.getItem('visiterCount')}`;
-}
-updateCounter();
 
 
-// for location element
-function updateLocation(){
     const locti = document.getElementById('locti');
     let location = localStorage.getItem('visiterLocation');
     if (location == null){
@@ -30,5 +27,7 @@ function updateLocation(){
     }
     localStorage.setItem('visiterLocation', location);
     locti.innerHTML = `Vistor Location: ${localStorage.getItem('visiterLocation')}`;
+
 }
-updateLocation();
+updateCounter();
+
